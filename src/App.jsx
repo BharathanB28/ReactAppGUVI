b3fa55856c154090e650e127cc30cfd2bc25604d
Bharathan <bharathan655@gmail.com>
2023-10-12 22:13:32 +0530

    Niranjan added

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ function Hello(props){
 function App() {
 
   let user1 = "Bharathan"
+  let user2 = "Niranjan"
   return(
     <div>
       <p>Greetings</p>
       <Hello userName={user1}/>
+      <Hello userName={user2}/>
       
     </div>
     
